7d04209b609784c77160993de82dfadccfd3062c
kat81 <katie@gull.net>
2014-01-10 14:23:30 -0500

    Added basic input functionality

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,10 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("Please enter password: ");
 
+		Scanner in = new Scanner(System.in);
+		String input = in.next();
+
+		if (input.equalsIgnoreCase("password")) {
+			System.out.println("Password correct!");
+		} else {
+			System.out.println("Password incorrect!");
+		}
 	}
-
 }
