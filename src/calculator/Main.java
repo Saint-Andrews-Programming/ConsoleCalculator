9063a0dd0953e87c889566e87513f76ec4432820
Tommsy64 <thomas.pakh@gmail.com>
2014-01-14 13:23:08 -0500

    Added a loop and exit to the User Interface

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -18,29 +18,31 @@ public class Main {
 		System.out.println();
 		System.out.println("Type exit to exit the calculator");
 
-		try {
+		Boolean run = true;
+		while (run) {
+			try {
+				String input = in.next();
 
-			String input = in.next();
+				if (input.equalsIgnoreCase("exit"))
+					System.exit(0);
 
-			if (input.equalsIgnoreCase("exit"))
-				System.exit(0);
+				List<IEquationPart> equationParts = Parser.parse(input);
 
-			List<IEquationPart> equationParts = Parser.parse(input);
+				for (IEquationPart part : equationParts) {
+					System.out.print(part.toString() + " ");
+				}
 
-			for (IEquationPart part : equationParts) {
-				System.out.print(part.toString() + " ");
+				System.out.print(" = ");
+
+				Equation equation = new Equation(equationParts);
+				for (IEquationPart value : equation.value()) {
+					System.out.print(value.toString() + " ");
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("An unknown error has accured!");
 			}
-
-			System.out.print(" = ");
-
-			Equation equation = new Equation(equationParts);
-			for (IEquationPart value : equation.value()) {
-				System.out.print(value.toString() + " ");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("An unknown error has accured!");
 		}
 	}
 }
